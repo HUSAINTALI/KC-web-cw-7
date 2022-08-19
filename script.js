eccf5d13e95a5729cb144ce7826fd5414da84031
HUSAINTALI <110049743+HUSAINTALI@users.noreply.github.com>
2022-08-19 18:08:38 +0300

    cw7
    done

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function BMI(weight,height){
+let result =weight / (height * height); 
+return result;
+}
+console.log(bmi(70,1.70));
 
+function status(bmi) 
+
+if (bmi < 18.5) {
+return ("لديك نقص في الوزن"	);
+}
+ else if (bmi >= 18.5 && bmi < 25) {
+return ("وزنك صحي"	);
+} else ( "لديك زيادة في الوزن"	)
+
+function calculate (){
+    
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+let bmi = BMI (weight, height);
+
+let desc = status(bmi);
+
+let div = document.getElementById('result');
+
+div.innerText = bmi + " == " + desc
+
+
+
+
+
+}
